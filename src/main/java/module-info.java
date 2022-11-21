4bef4bf07d26fffdda043fa3afadd46a5cfc2de9
MateoSilvaLasso <you@example.com>
2022-11-21 18:53:36 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.rappi_ando {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.rappi_ando to javafx.fxml;
+    exports com.example.rappi_ando;
+}
