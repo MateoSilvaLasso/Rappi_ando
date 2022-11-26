2313b0612d188b0c71a6f7c46de1dad227948cdf
Annsof <Annsof>
2022-11-25 21:10:29 -0500

    Graphic interface V1.5.2 Changes by Mateo

--- test/com/example/rappi_ando/graph_implementation_generics/AdjacencyListTest.java
+++ test/com/example/rappi_ando/graph_implementation_generics/AdjacencyListTest.java
@@ -17,42 +17,42 @@ class AdjacencyListTest{
     public void setUp2(){
         graph = new AdjacencyList<Integer>();
         //Adding the vertex
+        graph.addVertex(0, 0);
         graph.addVertex(1, 1);
         graph.addVertex(2, 2);
-        graph.addVertex(3, 3);
     }
 
     public void setUp3(){
         graph = new AdjacencyList<Integer>();
         //Adding the vertex
+        graph.addVertex(0, 0);
         graph.addVertex(1, 1);
         graph.addVertex(2, 2);
-        graph.addVertex(3, 3);
 
         //Adding the edges
-        graph.addEdge(1, 2 , 10);
-        graph.addEdge(2, 3 , 5);
-        graph.addEdge(3, 1 , 20);
+        graph.addEdge(0, 1 , 10);
+        graph.addEdge(1, 2 , 5);
+        graph.addEdge(2, 0 , 20);
     }
 
     public void setUp4(){
         graph = new AdjacencyList<Integer>();
         //Adding the vertex
+        graph.addVertex(0, 0);
         graph.addVertex(1, 1);
         graph.addVertex(2, 2);
-        graph.addVertex(3, 3);
 
         //Adding the edges
-        graph.addEdge(2, 1 , 10);
-        graph.addEdge(2, 3 , 5);
+        graph.addEdge(1, 0 , 10);
+        graph.addEdge(1, 2 , 5);
     }
 
-    void setUp5(){
+    public void setUp5(){
         graph = new AdjacencyList<Integer>();
         //Adding the vertex
-        graph.addVertex(0, 1);
-        graph.addVertex(1, 2);
-        graph.addVertex(2, 3);
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 1);
+        graph.addVertex(2, 2);
 
         //Adding the edges
         graph.addEdge(0, 1 , 10);
@@ -60,12 +60,28 @@ class AdjacencyListTest{
         graph.addEdge(2,0,20);
     }
 
+    public void setUp6(){
+        graph= new AdjacencyList<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 1);
+        graph.addVertex(2, 2);
+        graph.addVertex(3,3);
+        graph.addVertex(4,4);
+
+        //Adding the edges
+        graph.addEdge(0, 1 , 10);
+        graph.addEdge(1, 2 , 5);
+        graph.addEdge(2,0,20);
+        graph.addEdge(3,4,1);
+
+    }
+
     // Tests of the addVertex method
     @Test
     public void addVertexTest1() {
         setUp1();
-        graph.addVertex(1, 1);
-        assertEquals(graph.getGraph().size(), 2);
+        graph.addVertex(0, 0);
+        assertEquals(graph.getGraph().size(), 1);
     }
     @Test
     public void addVertexTest2() {
@@ -76,7 +92,7 @@ class AdjacencyListTest{
         }catch (Exception exception){
             exception.printStackTrace();
         }
-        assertEquals(graph.getGraph().size(), 2);
+        assertEquals(graph.getGraph().size(), 1);
     }
 
     @Test
@@ -92,22 +108,22 @@ class AdjacencyListTest{
     @Test
     public void addEdgeTest1(){
         setUp2();
-        graph.addEdge(3, 1, 10);
-        assertEquals(graph.searchNode(3).getNodes().get(0).getValue(), 10);
+        graph.addEdge(2,0, 10);
+        assertEquals(graph.searchNode(2).getNodes().get(0).getValue(), 10);
     }
     @Test
     public void addEdgeTest2(){
         setUp2();
-        graph.addEdge(3, 1, 10);
-        graph.addEdge(3,1, 20);
-        assertEquals(graph.searchNode(3).getNodes().get(1).getValue(), 20);
+        graph.addEdge(2, 0, 10);
+        graph.addEdge(2,0, 20);
+        assertEquals(graph.searchNode(2).getNodes().get(1).getValue(), 20);
     }
 
     @Test
     public void addEdgeTest3(){
         setUp2();
-        graph.addEdge(2, 1, -5);
-        assertEquals(graph.searchNode(2).getNodes().get(0).getValue(), -5);
+        graph.addEdge(1, 0, -5);
+        assertEquals(graph.searchNode(1).getNodes().get(0).getValue(), -5);
     }
 
     //Testing of the BFS method
@@ -115,11 +131,11 @@ class AdjacencyListTest{
     @Test
     public void BFSTest1() {
         setUp3();
-        ArrayList<Node<Integer>> test = graph.BFS(1);
+        ArrayList<Node<Integer>> test = graph.BFS(0);
         ArrayList<Integer> expectedNodes = new ArrayList<>();
+        expectedNodes.add(0);
         expectedNodes.add(1);
         expectedNodes.add(2);
-        expectedNodes.add(3);
         for (Node<Integer> n: test){
             expectedNodes.remove(n.getValue());
         }
@@ -134,10 +150,10 @@ class AdjacencyListTest{
     @Test
     public void BFSTest3() {
         setUp4();
-        ArrayList<Node<Integer>> test = graph.BFS(2);
-        assertEquals(test.get(0).getKey(), 2);
-        assertEquals(test.get(1).getKey(), 1);
-        assertEquals(test.get(2).getKey(), 3);
+        ArrayList<Node<Integer>> test = graph.BFS(1);
+        assertEquals(test.get(0).getKey(), 1);
+        assertEquals(test.get(1).getKey(), 0);
+        assertEquals(test.get(2).getKey(), 2);
     }
 
     // Tests for the Method DFS
@@ -145,8 +161,34 @@ class AdjacencyListTest{
 
     @Test
     public void DFStest1(){
-        setUp4();
+        setUp5();
         assertEquals(graph.DFS(),1);
+    }
+
+    @Test
+    public void DFStest2(){
+        setUp6();
+        assertEquals(graph.DFS(),2);
+    }
+
+    //Test for the method Dijkstra
+
+    @Test
+    public void Dijkstra1(){
+        setUp5();
+        ArrayList<Integer> a= graph.dijkstra(0);
+        assertEquals(a.get(0),0);
+        assertEquals(a.get(1),10);
+        assertEquals(a.get(2),15);
+    }
+
+    @Test
+    public void Dijkstra2(){
+        setUp5();
+        ArrayList<Integer> a= graph.dijkstra(1);
+        for(Integer v: a){
+            
+        }
     }
 
 }
